61d8b5f7e5d978090cd6f9194e623bd18c0041cf
LittleHaku <ivandelhorno@gmail.com>
2024-02-27 19:35:02 +0100

    switch button

---
+++ AndroidApp/Cards/app/src/main/java/es/uam/eps/dadm/cards/Card.kt
@@ -0,0 +1,98 @@
+import java.time.LocalDateTime
+import java.time.LocalDateTime.*
+import java.time.format.DateTimeFormatter
+import java.util.UUID
+import kotlin.math.roundToLong
+
+open class Card(
+    var question: String,
+    var answer: String,
+    var date: String = now().toString(),
+    var id: String = UUID.randomUUID().toString(),
+    var quality: Int = 0,
+    var repetitions: Int = 0,
+    var interval: Long = 1L,
+    var nextPracticeDate: String = date,
+    var easiness: Double = 2.5
+
+) {
+    companion object {
+        fun fromString(string: String): Card {
+            val parts = string.split("|").map { it.trim() }
+            val question = parts[1]
+            val answer = parts[2]
+            val date = parts[3]
+            val id = parts[4]
+            val quality = parts[5].toInt()
+            val repetitions = parts[6].toInt()
+            val interval = parts[7].toLong()
+            val nextPracticeDate = parts[8]
+            val easiness = parts[9].toDouble()
+
+            return Card(question, answer, date, id, quality, repetitions, interval, nextPracticeDate, easiness)
+        }
+    }
+
+
+    override fun toString(): String {
+        return "card | $question | $answer | $date | $id | $quality | $repetitions | $interval | $nextPracticeDate | $easiness"
+    }
+
+    open fun show() {
+        print("\n$question (ENTER to see answer)")
+        readln()
+        print("$answer (Type 0 -> Difficult 3 -> Doubt 5 -> Easy): ")
+        val quality = readlnOrNull()?.toIntOrNull() ?: -1
+        if (quality == 0 || quality == 3 || quality == 5)
+            this.quality = quality
+        else
+            println("Not a valid difficulty")
+    }
+
+    fun update(currentDate: LocalDateTime) {
+        // easiness
+        val newEasiness = easiness + 0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02)
+        easiness = if (newEasiness < 1.3) 1.3 else newEasiness
+
+        // reps
+        repetitions = if (quality < 3) 0 else repetitions + 1
+
+
+        // interval
+        interval = when (repetitions) {
+            0, 1 -> 1
+            2 -> 6
+            else -> (interval * easiness).roundToLong()
+        }
+
+        // date
+        nextPracticeDate = currentDate.plusDays(interval).toString()
+    }
+
+    fun details() {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val date = parse(nextPracticeDate).format(formatter)
+
+        println(
+            "eas = %.2f rep = $repetitions int = $interval next = $date".format(easiness)
+        )
+    }
+
+    fun simulate(period: Long) {
+        println("Simulation of the card $question:")
+        var now = now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+        for (i in 1..period) {
+            println("Date: ${now.format(formatter)}")
+            if (now >= parse(nextPracticeDate)) {
+                show()
+                update(now)
+                details()
+            }
+            now = now.plusDays(1)
+        }
+    }
+
+
+}
--- AndroidApp/Cards/app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
+++ AndroidApp/Cards/app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
@@ -1,16 +1,35 @@
 package es.uam.eps.dadm.cards
 
+import Card
+import android.graphics.Paint.Style
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import es.uam.eps.dadm.cards.ui.theme.CardsTheme
+import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +37,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             CardsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CardItem(Card("To wake up", "Despertarse"))
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -40,4 +63,48 @@ fun GreetingPreview() {
     CardsTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun CardItem(
+    card: Card,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Top
+    ) {
+        var switchState: Boolean by remember { mutableStateOf(false) }
+
+        Column {
+            Text(
+                card.question,
+                modifier,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(card.answer, modifier, style = MaterialTheme.typography.bodyMedium)
+            if (switchState) {
+                Text("Quality = ${card.quality}\nEasiness = ${card.easiness}" +
+                        "\nRepetitions = ${card.repetitions}", modifier, style = MaterialTheme.typography.bodySmall)
+            }
+        }
+
+        Column() {
+            Text(card.date.toString().substring(0..9))
+            Switch(
+                checked = switchState,
+                onCheckedChange = { switchState = it }
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Screen() {
+    CardItem(Card("To wake up", "Despertarse")
+    )
 }
